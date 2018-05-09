2705e6fe73cb03176c8f397b307e55a1faaad276
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:22:15 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 function addElementToBeginningOfArray(array, element) {
-  [element, ...array];
+  return [element, ...array];
 }
 
 
