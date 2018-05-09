8b195a5791d3d74a585c6fee05eb9598773de210
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:50:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,11 +8,8 @@ var destructivelyAddElementToEndOfArray = (array, element) => {
   array.push(element);
   return array;
 }
-var destructivelyRemoveElementFromBeginningOfArray = (array) => array.shift();
-var removeElementFromBeginningOfArray = (array) => {
-  array.slice(1);
-  return array;
-}
+var destructivelyRemoveElementFromBeginningOfArray = (array) => {array.shift();array;}
+var removeElementFromBeginningOfArray = (array) => array.slice(1);
 var removeElementFromEndOfArray = (array) => array.slice(0, array.length-1);
 var destructivelyRemoveElementFromEndOfArray = (array) => {
   array.pop();
