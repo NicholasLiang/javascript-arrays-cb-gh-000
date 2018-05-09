b76bc38e75cf01ccbb5888c68aad181d1fb537c4
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:37:26 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 var addElementToBeginningOfArray = (array, element) => [element, ...array];
-var destructivelyAddElementToBeginningOfArray = (array, element) => array.push(element);
+var destructivelyAddElementToBeginningOfArray = (array, element) => array.unshift(element);
 var addElementToEndOfArray = (array, element) => [...array, element];
 var destructivelyAddElementToEndOfArray = (array, element) => array.push(element);
 var destructivelyRemoveElementFromBeginningOfArray = (array) => array.shift();
