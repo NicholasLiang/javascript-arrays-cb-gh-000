50170a4113721ab068b8476ba4f04f9991f72b90
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:26:35 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,44 +4,6 @@ var destructivelyAddElementToBeginningOfArray = (array, element) => array.push(e
 
 
 
-// describe('arrays', () => {
-//   jsdom({
-//     src: fs.readFileSync(path.resolve(__dirname, '..', 'arrays.js'), 'utf-8')
-//   })
-
-//   describe('chocolateBars', () => {
-//     it('is an array containing "snickers", "hundred grand", "kitkat", and "skittles"', () => {
-//       expect(chocolateBars).to.eql['snickers', 'hundred grand', 'kitkat', 'skittles']
-//     })
-//   })
-
-//   describe('addElementToBeginningOfArray(array, element)', () => {
-//     it('adds an element to the beginning of an array', () => {
-//       expect(addElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-//     })
-
-//     it('does not alter the original array', () => {
-//       const array = [1]
-
-//       addElementToBeginningOfArray(array, 'foo')
-
-//       expect(array).to.eql([1])
-//     })
-//   })
-
-//   describe('destructivelyAddElementToBeginningOfArray(array, element)', () => {
-//     it('adds an element to the beginning of an array', () => {
-//       expect(destructivelyAddElementToBeginningOfArray([1], 'foo')).to.eql(['foo', 1])
-//     })
-
-//     it('alters the original array', () => {
-//       const array = [1]
-
-//       destructivelyAddElementToBeginningOfArray(array, 'foo')
-
-//       expect(array).to.eql(['foo', 1])
-//     })
-//   })
 
 //   describe('addElementToEndOfArray(array, element)', () => {
 //     it('adds an element to the end of an array', () => {
