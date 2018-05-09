3b623c634e8ffa958f297893fd591768407c20aa
Nicholas Liang <ssss021418@gmail.com>
2018-05-09 14:22:41 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,5 @@
 chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
-function addElementToBeginningOfArray(array, element) {
-  return [element, ...array];
-}
+function addElementToBeginningOfArray = (array, element) = [element, ...array];
 
 
 // describe('arrays', () => {
